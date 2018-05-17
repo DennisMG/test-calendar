e56ad2b3b82dadcb0e5f48b4ff7b347081b1c047
Dennis Molina <dennismolina.17@gmail.com>
2018-05-17 13:19:32 -0600

    Added input component

---
+++ src/InputComponent/InputComponent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './InputComponent.css'
+
+const InputComponent = (props) => {	
+
+	return(
+		<div className="InputComponent">
+		</div>
+	) 
+}
+
+export default InputComponent;
