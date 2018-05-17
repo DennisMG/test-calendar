31b3db71e90908dcdbf580f4f16c166026c3268e
Dennis Molina <dennismolina.17@gmail.com>
2018-05-17 13:32:28 -0600

    Added user inputs and some minor styling

--- src/InputComponent/InputComponent.js
+++ src/InputComponent/InputComponent.js
@@ -6,6 +6,15 @@ const InputComponent = (props) => {
 
 	return(
 		<div className="InputComponent">
+			<div>
+				Start Date: <input type="text" onChange={props.changed} value={props.name}/>
+			</div>
+			<div>
+				Number of days: <input type="text" onChange={props.changed} value={props.name}/>
+			</div>
+			<div>
+				Country Code: <input type="text" onChange={props.changed} value={props.name}/>
+			</div>
 		</div>
 	) 
 }
---
+++ src/Month/Month.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './Month.css'
+
+const Month = (props) => {
+	return(
+		<div className="Month">
+		</div>
+	) 
+}
+
+export default Month;
