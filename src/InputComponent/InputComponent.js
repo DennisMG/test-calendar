257085914e3f75c4709b206d773fef3a64461f70
Dennis Molina <dennismolina.17@gmail.com>
2018-05-18 14:27:19 -0600

    Styling input components

--- src/InputComponent/InputComponent.js
+++ src/InputComponent/InputComponent.js
@@ -5,20 +5,20 @@ import './InputComponent.css'
 const InputComponent = (props) => {	
 
 	return(
-		<div className="InputComponent">
-			<div>
+		<div className="inputComponent">
+			<div className="input">
 				Start Date: <input 
 								type="date" 
 								onChange={props.onStartDateChanged}/>
 			</div>
-			<div>
+			<div className="input">
 				Number of days: <input 
 								  type="number" 
 								  onChange={props.onNumberOfDaysChanged}
 								  min="1"
 								  value={props.numberOfDays}/>
 			</div>
-			<div>
+			<div className="input">
 				Country Code: <input 
 								type="text" 
 								onChange={props.changed} 
